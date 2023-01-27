23b5df225b9da2026b6dc01b88f1b227946cf329
odilovshahzod <odilovshahzod1997@gmail.com>
2023-01-27 18:49:08 +0500

    UNIVER

--- script.js
+++ script.js
@@ -3,35 +3,30 @@ window.addEventListener('DOMContentLoaded',()=>{
         const header=document.querySelector('header');
         header.classList.toggle('sticky',window.scrollY>0)
     })
-   
+
+    menu_open=document.querySelector('.open');
+    menu_close=document.querySelector('.close');
+    navbar=document.querySelector('.navbar');
+    navbar_open_close_click=document.querySelector('.menu_navbar .span_container');
+    menu_open.addEventListener('click',()=>{
+        navbar.classList.add('open_navbar_menu');
+        navbar_open_close_click.style.cssText=`transform: translateY(-30px);`
+    })
+    menu_close.addEventListener('click',()=>{
+        navbar.classList.remove('open_navbar_menu');
+        navbar_open_close_click.style.cssText=`transform: translateY(0);`
+    })
     const slides=document.querySelectorAll('.img'),
     slidesWrapper=document.querySelector('.cosmo_img'),
     slidesField=document.querySelector('.img_block'),
     leftButton=document.querySelector('#leftButton'),
     rightButton=document.querySelector('#rightButton'),
     width=getComputedStyle(slidesWrapper).width;
-  
-    const menuBtn=document.querySelector('.menu-btn');
-    const navigation = document.querySelector('.navbar');
-    const navigationIteams = document.querySelectorAll('.navbar a');
-    menuBtn.addEventListener('click',()=>{
-        menuBtn.classList.toggle('active')
-        navigation.classList.toggle('active')
-    })
-    navigationIteams.forEach(navIteam =>{
-        navIteam.addEventListener('click',()=>{
-            menuBtn.classList.remove('active')
-            navigation.classList.remove('active')
-        })
-    })
 
-    let index=1;
     let offset=0;
-    // slidesField.style.width=100*slides.length+'%';
     slides.forEach(slide=>{
         slide.style.width=width 
     })
-    
     rightButton.addEventListener('click',()=>{
         if(offset==(+width.slice(0,width.length-2)*(slides.length-1))){
             offset=0;
@@ -39,7 +34,6 @@ window.addEventListener('DOMContentLoaded',()=>{
         else{offset += +width.slice(0,width.length-2)}
         slidesWrapper.style.transform=`translateX(-${offset}px)`
     })
-
     leftButton.addEventListener('click',()=>{
     if(offset==0){
         offset = (+width.slice(0,width.length-2)*(slides.length-1));
@@ -48,4 +42,63 @@ window.addEventListener('DOMContentLoaded',()=>{
     slidesWrapper.style.transform=`translateX(-${offset}px)`
 
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// -------------------------------------------------
+
+    // const slides=document.querySelectorAll('.img'),
+    // slidesWrapper=document.querySelector('.cosmo_img'),
+    // slidesField=document.querySelector('.img_block'),
+    // leftButton=document.querySelector('#leftButton'),
+    // rightButton=document.querySelector('#rightButton'),
+    // width=getComputedStyle(slidesWrapper).width;
+
+    // let index=1;
+    // let offset=0;
+    // slidesField.style.width=100*slides.length+'%';
+//     slides.forEach(slide=>{
+//         slide.style.width=width 
+//     })
+//     console.log(width);
+    
+//     rightButton.addEventListener('click',()=>{
+//         console.log(slides.);
+//         if(offset==(+width.slice(0,width.length-2)*(slides.length-1))){
+//             offset=0;
+//         }
+//         else{offset += +width.slice(0,width.length-2)}
+//         slidesWrapper.style.transform=`translateX(-${offset}px)`
+//     })
+//     console.log('ofst='+offset);
+
+//     leftButton.addEventListener('click',()=>{
+//     if(offset==0){
+//         offset = (+width.slice(0,width.length-2)*(slides.length-1));
+//     }
+//     else{offset -= +width.slice(0,width.length-2)}
+//     slidesWrapper.style.transform=`translateX(-${offset}px)`
+
+// })
 })
